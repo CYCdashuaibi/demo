0c946475685f6bc6aa7455b2aba9a2ce6078bf48
chenyc <chenyc@gzsendi.cn>
2025-05-26 16:57:30 +0800

    1

--- octopus-erp/js/apis.js
+++ octopus-erp/js/apis.js
@@ -2,10 +2,11 @@
 // 获取合同列表
 export const contractSupplierListApi = "/octopus/portal/erp/contractSupplier/list";
 
+// 获取合同主体列表
+export const getCompanySubjectApi = "/octopus/portal/erp/contractSupplier/getCompanySubject";
+
+// 获取供应商列表
 export const queryContractListApi = "/octopus/portal/erp/contractSupplier/queryContractList";
 
 // 获取用户信息
 export const getPersonInfoApi = "/octopus/portal/erp/contractSupplier/getPersonInfo";
-
-// 获取公司主体
-export const getCompanySubjectApi = "/octopus/portal/erp/contractSupplier/getCompanySubject";
